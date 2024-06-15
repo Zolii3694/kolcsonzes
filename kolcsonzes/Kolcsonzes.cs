079f74dc659a0627658c3ce989cceb32d4309a7a
Zolii6767 <155897924+Zolii3694@users.noreply.github.com>
2024-06-15 11:09:08 +0200

    Add project files.

---
+++ kolcsonzes/Kolcsonzes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VizibicikliKolcsonzes
+{
+    class Kolcsonzes
+    {
+
+        public string nev, azonosito;
+        public int eOra, ePerc, eIdo, vOra, vPerc, vIdo ;
+        
+        
+        public Kolcsonzes(string sor) {
+            var tmp = sor.Split(';');
+
+            nev = tmp[0];
+            azonosito = tmp[1];
+            eOra = Convert.ToInt32(tmp[2]);
+            ePerc = Convert.ToInt32(tmp[3]);
+            eIdo = eOra * 60 + ePerc;
+            vOra = Convert.ToInt32(tmp[4]);
+            vPerc = Convert.ToInt32(tmp[5]);
+            vIdo = vOra * 60 + vPerc;
+
+
+        }
+    }
+}
---
+++ kolcsonzes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics.Tracing;
+using System.Runtime.InteropServices;
+
+namespace VizibicikliKolcsonzes
+{
+    class Program
+    {
+   
+
+        static void Main(string[] args)
+        {        
+            List<Kolcsonzes> kolcsonzesek = new List<Kolcsonzes>();
+
+            File.ReadAllLines("kolcsonzesek.txt").Skip(1).ToList().ForEach(item => kolcsonzesek.Add(new Kolcsonzes(item)));
+
+            Console.WriteLine($"5. feladat: Napi kolcsonzesek szama: {kolcsonzesek.Count}");
+
+            Console.Write("6. feladat: Kerek egy nevet: ");
+            string megadottNev = Console.ReadLine();
+            bool kolcsonzott = false;
+            foreach (Kolcsonzes item in kolcsonzesek)
+            {
+                if (item.nev == megadottNev)
+                {
+                    if (!kolcsonzott)
+                    {
+                        Console.WriteLine("\t" + megadottNev + " kolcsonzesei:");
+                        Console.WriteLine("\t" + item.eOra + ":" + item.ePerc + " - " + item.vOra + ":" + item.vPerc);
+                        kolcsonzott = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\t" + item.eOra + ":" + item.ePerc + " - " + item.vOra + ":" + item.vPerc);
+                    }
+                }
+            }
+            if (!kolcsonzott) { Console.WriteLine("Nem volt ilyen nevu kolcsonzo. "); }
+
+            Console.ReadKey();
+        }
+    }
+}
